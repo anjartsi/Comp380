b26daea147f30452aaed0b5cb9bf42c4701e0ef6
paulc9 <lobos4646@gmail.com>
2016-10-25 21:29:38 -0700

    Rename code.js to script.js, Move randomNum function to globalFunctions.js

--- Chapter1/problems.js
+++ Chapter1/problems.js
@@ -1,17 +1,1 @@
-var number = 0;
-
-function randomNum(min, max){
-	// This function combines the randomInt() and randomNotReally() functions in
-	// globalFunctions.js to generate a random number in the range between min and max inclusive.
-	// wait is the number of milliseconds it takes to change the result. 1000 ms = 1 s.
-	var wait = 5000;
-	var result;
-  	var d1 = new Date().getTime();
- 	var d2 = new Date().getTime() % wait;
- 	d1 = d1 - d2;
- 	d1 = Math.sin(d1);
- 	d1 = d1 - Math.floor(d1);
- 	result = Math.floor(((min + 0.5) + (max - min) * d1));
- 	return result;
-}
 document.getElementById("num").innerHTML = randomNum(0, 10);
--- globalFunctions.js
+++ globalFunctions.js
@@ -17,6 +17,21 @@ var randomInt = function(min, max){
   return Math.floor((min + 0.5) + (max - min) * randomNotReally());
 }
 
+function randomNum(min, max){
+	// This function combines the randomInt() and randomNotReally() functions in
+	// globalFunctions.js to generate a random number in the range between min and max inclusive.
+	// wait is the number of milliseconds it takes to change the result. 1000 ms = 1 s.
+	var wait = 5000;
+	var result;
+  	var d1 = new Date().getTime();
+ 	var d2 = new Date().getTime() % wait;
+ 	d1 = d1 - d2;
+ 	d1 = Math.sin(d1);
+ 	d1 = d1 - Math.floor(d1);
+ 	result = Math.floor(((min + 0.5) + (max - min) * d1));
+ 	return result;
+}
+
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ HTML Class Functions~~~~~~~~~~~~~~~
 function addClass(el, cls) {
   var classes = el.className.split(' ');
