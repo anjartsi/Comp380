23fe156ef0e5877a7fae64f14517f3e144e61822
paulc9 <lobos4646@gmail.com>
2016-11-22 14:26:58 -0800

    Start work on ch. 3

---
+++ chapter3/problems.js
@@ -0,0 +1,44 @@
+/* Problem 1: */
+var prob1 = document.getElementById("prob1");
+var output1 = document.getElementById("output1");
+prob1.innerHTML = problem1();
+var x1;
+var y1;
+var answer1;
+var hint1 = "";
+
+function problem1(){
+	var question1 = "";
+	question1 = "";
+	return question1;
+}
+
+/* Problem 2: */
+var prob2 = document.getElementById("prob2");
+var output2 = document.getElementById("output2");
+prob2.innerHTML = problem2();
+var t2;
+var x2;
+var answer2;
+var hint2 = "";
+
+function problem2(){
+	var question2 = "";
+	question2 = "";
+	return question2;
+}
+
+/* Problem 3: */
+var prob3 = document.getElementById("prob3");
+var output3 = document.getElementById("output3");
+prob3.innerHTML = problem3();
+var t3;
+var x3;
+var answer3;
+var hint3 = "";
+
+function problem3(){
+	var question3 = "";
+	question3 = "";
+	return question3;
+}
