dd3032f08f2f920d444db02573a8063c1e08d1bf
Armen Ourfalian <aourfalian@gmail.com>
2016-12-03 22:23:31 -0800

    Home page Complete

--- home-Page/script.js
+++ home-Page/script.js
@@ -5,6 +5,6 @@ var ch2Sect = document.getElementById("ch2Sect");
 var ch3 = document.getElementById("ch3");
 var ch3Sect = document.getElementById("ch3Sect");
 
-makeCollapsable(ch1, ch1Sect);
-makeCollapsable(ch2, ch2Sect);
-makeCollapsable(ch3, ch3Sect);
+// makeCollapsable(ch1, ch1Sect);
+// makeCollapsable(ch2, ch2Sect);
+// makeCollapsable(ch3, ch3Sect);
